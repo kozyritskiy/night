ad7c1774b1bf656fb0f9d375b071d2d484d6c644
Max Kozyritsky <maxim.kozyricki@gmail.com>
2018-06-12 08:53:34 +0300

    add footer

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -1,10 +1,12 @@
 module.exports = {
   white: "#ffffff",
-  beige: "#eeebdf",
+  beige: "#ebe9e3",
   green: "#16c2aa",
   dark: "black",
   "text-color": "#455a64",
-  grey: "#373e42",
+  grey: "#9e9fa3",
   "light-grey": "#dfdcd5",
-  blue: "#004ad2"
+  blue: "#004ad2",
+  beigelight: "#f2f0e9",
+  bluecircle: "#004cd1"
 };
